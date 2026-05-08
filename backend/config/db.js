d31dcfddf7534197d59858b185a75d90ea1990e5
g.srinivas <srinivasgunti90300@gmail.com>
2026-05-08 13:01:31 +0530

    Debug environment variables and force DB url in Prisma

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,16 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+console.log("=== DEBUG ENVIRONMENT VARIABLES ===");
+console.log("DATABASE_URL is defined:", !!process.env.DATABASE_URL);
+console.log("PORT is defined:", !!process.env.PORT);
+
+// Optionally pass it directly to be absolutely sure Prisma sees it
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
+    }
+  }
+});
 
 module.exports = prisma;
